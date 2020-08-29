08e9a52c6903fdd46ada484815b5d61091611a5f
Матковський Ростик <rostik.matkovskiy27@gmail.com>
2020-08-29 23:41:49 +0300

    fix

---
+++ Soccen/Views/ServiceExecutionPage.xaml.cs
@@ -0,0 +1,90 @@
+﻿using Soccen.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Soccen.Views
+{
+    /// <summary>
+    /// Interaction logic for ServiceExecutionPage.xaml
+    /// </summary>
+    public partial class ServiceExecutionPage : Page
+    {
+
+        soccenEntities context = new soccenEntities();
+        CollectionViewSource serviceexecutionViewSource;
+        CollectionViewSource servicesViewSource;
+
+        public ServiceExecutionPage()
+        {
+            InitializeComponent();
+            serviceexecutionViewSource = ((CollectionViewSource)(FindResource("serviceexecutionViewSource")));
+            servicesViewSource = ((CollectionViewSource)(FindResource("servicesViewSource")));
+            DataContext = this;
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            context.serviceexecutions.Load();
+            context.services.Load();
+            serviceexecutionViewSource.Source = context.serviceexecutions.Local;
+            servicesViewSource.Source = context.services.Local;
+        }
+
+        private void Page_Unloaded(object sender, RoutedEventArgs e)
+        {
+            context.SaveChanges();
+        }
+
+        private void LastCommandHandler(object sender, RoutedEventArgs e)
+        {
+            serviceexecutionViewSource.View.MoveCurrentToLast();
+        }
+
+        private void PreviousCommandHandler(object sender, RoutedEventArgs e)
+        {
+            serviceexecutionViewSource.View.MoveCurrentToPrevious();
+        }
+
+        private void NextCommandHandler(object sender, RoutedEventArgs e)
+        {
+            serviceexecutionViewSource.View.MoveCurrentToNext();
+        }
+
+        private void FirstCommandHandler(object sender, RoutedEventArgs e)
+        {
+            serviceexecutionViewSource.View.MoveCurrentToFirst();
+        }
+
+        private void SaveComandHandler(object sender, ExecutedRoutedEventArgs e)
+        {
+            context.SaveChanges();
+        }
+
+
+
+
+        //private void dgsalesinvoice_layoutupdated(object sender, eventargs e)
+        //{
+        //    thickness t = lbltotal.margin;
+        //    t.left = (serviceexecutiongrid.columns[0].actualwidth + 7);
+        //    lbltotal.margin = t;
+        //    lbltotal.width = serviceexecutiongrid.columns[1].actualwidth;
+
+        //    lbltotalsalesinvoiceamount.width = serviceexecutiongrid.columns[2].actualwidth;
+        //}
+    }
+}
